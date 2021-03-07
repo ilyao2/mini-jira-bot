b2a32dd9def5b92b07b88d0a81428031d6f824e1
Cyber <ilya.obydennye@gmail.com>
2021-03-07 22:54:33 +0700

    Refactoring

--- bot/handlers_manager/manager.py
+++ bot/handlers_manager/manager.py
@@ -1,4 +1,5 @@
 """Класс для управления обработчиками"""
+from telebot import TeleBot
 from bot.handlers_manager.command_manager import CommandManager
 from bot.handlers_manager.message_manager import MessageManager
 from bot.handlers_manager.callback_manager import CallbackManager
@@ -7,7 +8,7 @@ __author__ = 'Обыденный И.В.'
 
 
 class Manager:
-    def __init__(self, bot):
+    def __init__(self, bot: TeleBot):
         self.bot = bot
         self.command_manager = CommandManager(bot)
         self.message_manager = MessageManager(bot)
